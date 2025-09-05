8a627d2911fbb9df3428b3e4a2bdeb44dd597deb
0xhsn <mail@7asan.io>
2025-09-05 21:54:36 +0100

    fix build errors

--- poll/components/ui/avatar.tsx
+++ poll/components/ui/avatar.tsx
@@ -7,7 +7,7 @@ export function Avatar({ className, size = 36, alt = "", src, ...rest }: Props) 
   return (
     <img
       className={cn("rounded-full object-cover", className)}
-      style={{ width: size, height: size, WebkitUserDrag: "none", userSelect: "none" as const, pointerEvents: "none" as const }}
+      style={{ width: size, height: size, userSelect: "none" as const, pointerEvents: "none" as const }}
       width={size}
       height={size}
       src={src ?? ""}
