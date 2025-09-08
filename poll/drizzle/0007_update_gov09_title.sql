40a4dce193a858658cdd1f5c6505ffe3eff65f2b
0xhsn <mail@7asan.io>
2025-09-08 21:55:08 +0100

    fix: governors titles

---
+++ poll/drizzle/0007_update_gov09_title.sql
@@ -0,0 +1,6 @@
+-- Update title for governor with id 'gov09'
+UPDATE candidates
+SET title = 'محافظ السويداء (ريموت)'
+WHERE id = 'gov09';
+
+
---
+++ poll/drizzle/0008_update_gov11_title.sql
@@ -0,0 +1,6 @@
+-- Update title for governor with id 'gov11'
+UPDATE candidates
+SET title = 'محافظ القنيطرة (بارت تايم)'
+WHERE id = 'gov11';
+
+
