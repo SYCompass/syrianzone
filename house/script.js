45cc44018aaf23cec1191774bd993cc8322a0bf0
0xhsn <mail@7asan.io>
2025-09-19 20:44:17 +0200

    feat(house): add deierezzor

--- house/script.js
+++ house/script.js
@@ -24,6 +24,7 @@
     { key: 'tartus', label: 'طرطوس', sheetId: '1bZKrmEUiFHdeID8pXHkT8XBaZ--oo6g2mGNcVvZMCgc', gid: '1926010966' },
     { key: 'homs', label: 'حمص', sheetId: '1bZKrmEUiFHdeID8pXHkT8XBaZ--oo6g2mGNcVvZMCgc', gid: '252895295' },
     { key: 'aleppo', label: 'حلب', sheetId: '1bZKrmEUiFHdeID8pXHkT8XBaZ--oo6g2mGNcVvZMCgc', gid: '1121899715' },
+    { key: 'deir-ez-zor', label: 'دير الزور', sheetId: '1bZKrmEUiFHdeID8pXHkT8XBaZ--oo6g2mGNcVvZMCgc', gid: '1166128088' },
   ];
 
   function csvUrlFor(p){
