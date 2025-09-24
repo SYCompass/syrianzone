744bbd221c7c44511f7e2843105d4c54667f8492
0xhsn <mail@7asan.io>
2025-09-24 12:02:32 +0200

    fix build

--- poll/lib/arcjet.ts
+++ poll/lib/arcjet.ts
@@ -1,12 +1,14 @@
-import arcjet, { fixedWindow } from "@arcjet/sdk";
+import arcjet, { tokenBucket } from "@arcjet/next";
 
-// Arcjet client with simple 1 request per minute rule
+// Arcjet client with simple 1 request per minute rule (per IP)
 export const aj = arcjet({
   key: process.env.ARCJET_KEY || "",
   rules: [
-    fixedWindow({
-      max: 1,
-      window: "1m",
+    tokenBucket({
+      mode: "LIVE",
+      refillRate: 1, // 1 token per interval
+      interval: 60,  // seconds
+      capacity: 1,   // no burst
     }),
   ],
 });
